e86fa0c1724c781d17416817191b80882ce2989e
Tostadora1 <maau.98@gmail.com>
2019-10-12 18:00:11 -0300

    #108 ooooooooooooooooooooooooooooooooooo

--- Taller1/src/vista/IVista.java
+++ Taller1/src/vista/IVista.java
@@ -1,5 +1,8 @@
 package vista;
 
 public interface IVista {
+    /**Se encarga de imprimir en la vista informacion sobre el funcionamiento del programa
+     * @param texto: el texto a escribir
+     */
     void imprimirEnConsola(String texto);
 }
